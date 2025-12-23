cfefd04e0624a43d08974b78c746b8fa23855a05
Prince Kakadiya <kakadiyaprince207@gmail.com>
2025-12-23 19:05:13 +0530

    Create lc_1910_remove_all_occurrences_of_a_substring.cpp

---
+++ Strings/lc_1910_remove_all_occurrences_of_a_substring.cpp
@@ -0,0 +1,28 @@
+/*
+LeetCode 1910
+Remove All Occurrences of a Substring
+
+Time Complexity: O(n * m)   // n = s.size(), m = part.size()
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string res = "";
+        int m = part.size();
+
+        // build result string and remove part when found
+        for (char c : s) {
+            res.push_back(c);
+
+            // check if the last m characters match part
+            if (res.size() >= m &&
+                res.substr(res.size() - m) == part) {  //check if last m charcters are match to the part
+                res.erase(res.size() - m);  // remove matched part
+            }
+        }
+
+        return res;
+    }
+};
