d6a75f0133e3019df978dcad7daf934a9ebdc7b0
Prince Kakadiya <kakadiyaprince207@gmail.com>
2025-12-26 17:19:02 +0530

    Create prime_number.cpp

---
+++ Math/prime_number.cpp
@@ -0,0 +1,30 @@
+/*
+Prime Number Check
+
+Time Complexity: O(√n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPrime(int n) {
+
+        // numbers less than 2 are not prime
+        if (n <= 1)
+            return false;
+
+        // check divisibility from 2 to √n
+        for (int i = 2; i * i <= n; i++) {
+
+            // if divisible, not a prime number
+            if (n % i == 0)
+                return false;
+        }
+
+        // no divisors found → prime
+        return true;
+    }
+};
