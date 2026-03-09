2151612951c2c3eea8df9a457af84c5108b3250e
Prince Kakadiya <kakadiyaprince207@gmail.com>
2026-03-09 14:01:50 +0530

    added the lc_238_product_of_array_except_self.cpp

---
+++ Arrays/lc_238_product_of_array_except_self.cpp
@@ -0,0 +1,34 @@
+/*
+LeetCode 238
+Product of Array Except Self
+
+Time Complexity: O(n)
+Space Complexity: O(1)  // excluding output array
+*/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> ans(n, 1);
+
+        int left = 1;
+
+        // prefix product
+        for (int i = 0; i < n; i++) {
+            ans[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+
+        // suffix product
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= right;
+            right *= nums[i];
+        }
+
+        return ans;
+    }
+};
